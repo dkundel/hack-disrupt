1e8a010ae6e327d3d8d51e82eae930b3d51c2c64
Lukasz Koprowski <azram19@gmail.com>
2015-12-05 22:37:49 +0000

    Add /user route for mongodb insert

--- server/api/definition.js
+++ server/api/definition.js
@@ -1,21 +1,56 @@
 module.exports =
 {
   "routes": [{
+	  "type": "POST",
+    "url": "/user/",
+	"handles": [
+		{
+		"module": "mongodb-insert",
+		"config": {
+			"configuration": {
+				"hostname": "{{env.hostname}}",
+				"port": "{{env.port}}",
+				"database": "{{env.database}}",
+				"collection": "testUsers"
+			},
+			"params": {
+				"email": "{{value.email}}",
+				"name": "{{value.name}}"
+			}
+		}
+		}]},
+ 	{
     "type": "POST",
     "url": "/example/:id",
-	"handles": [{
-	"module": "twilio",
-	"config": {
-		"configuration": {
-			"accountSid": "{{env.accountSid}}",
-			"authToken": "{{env.authToken}}"
-		},
-		"params": {
-			"from": "{{value.from}}",
-			"to": "{{value.to}}",
-			"body": "Hi, {{value.name}}! How are you?"
+	"handles": [
+		{
+		"module": "mongodb",
+		"config": {
+			"configuration": {
+				"hostname": "{{env.hostname}}",
+				"port": "{{env.port}}",
+				"database": "{{env.database}}",
+				"collection": "{{value.collection}}"
+			},
+			"params": {
+				"email": "{{value.email}}"
+			}
 		}
-	}
-	}]
+		},
+		{
+		"module": "twilio",
+		"config": {
+			"configuration": {
+				"accountSid": "{{env.accountSid}}",
+				"authToken": "{{env.authToken}}"
+			},
+			"params": {
+				"from": "{{value.from}}",
+				"to": "{{value.to}}",
+				"body": "Hi, {{value.name}}! How are you?"
+			}
+		}
+		}
+	]
     }]
   }
